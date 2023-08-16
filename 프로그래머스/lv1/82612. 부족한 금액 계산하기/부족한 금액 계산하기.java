8c46202ed36dce5f4cb2183a7d944909c6b2a0c5
dodhddl <132051151+dodhddl@users.noreply.github.com>
2023-08-16 21:56:42 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.09 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -1,8 +1,12 @@
 class Solution {
     public long solution(int price, int money, int count) {
-       
-        long total = price * (long)count * (count + 1) / 2; // 가우스의 덧셈 공식 사용
-        if (money < total) {
+      
+        long total = 0; // long으로 변경하여 큰 값에 대응
+        for (int i = 1; i <= count; i++) {
+            total += price * i; // 각 구매 횟수별 가격을 누적 계산
+        }
+        
+        if (total > money) {
             return total - money;
         } else {
             return 0;
